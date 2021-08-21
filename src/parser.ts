eec4f3aca75ab6e2ab005cceca1d144df642855d
hogashi <hogextend@gmail.com>
2021-08-22 04:26:52 +0900

    fix regexp

--- src/parser.ts
+++ src/parser.ts
@@ -1,64 +1,86 @@
+// 便利関数たち
+/** `/foo?\nbar/` から `"foo?\\nbar"` をつくる */
 const regExpToString = (regExp: RegExp): string =>
   regExp.toString().slice(1, -1);
+/**
+ * 複数の正規表現を組み合わせてひとつの正規表現にする(まとめてグループ化される(キャプチャなし))
+ * - `r(/foo?\nbar/, /|/, /(baz)*\s/)` --> `/(?:foo?\nbar|(baz)*\s)/`
+ */
 const r = (...patterns: RegExp[]): RegExp => {
   return new RegExp(
     [
-      '(',
+      '(?:',
       patterns.map((pattern) => regExpToString(pattern)).join(''),
       ')',
     ].join('')
   );
 };
-/** rrepeat(/a/, '+') => /a+/ */
+/**
+ * `r()` のキャプチャあり版
+ * - `rcapture(/foo?\nbar/, /|/, /(baz)*\s/)` --> `/((?:foo?\nbar|(baz)*\s))/`
+ * - `r()` を使うので内側に1つグループを余計につくる(内側のはキャプチャなしなので意味はない)
+ */
+const rcapture = (...patterns: RegExp[]): RegExp => {
+  return new RegExp(`(${regExpToString(r(...patterns))})`);
+};
+/**
+ * 正規表現の末尾に繰り返し記号をつける
+ * - `rrepeat(/foo/, '?')` --> `/foo?/`
+ * - `rrepeat(/b(ar)/, '+')` --> `/b(ar)+/`
+ */
 const rrepeat = (pattern: RegExp, repeatStr: string): RegExp =>
   new RegExp(regExpToString(pattern) + repeatStr);
 
-// regexp patterns
+// パターンたち
 const or = /|/;
+/** クオート */
 const q = /['"]/;
+/** クオート以外の連続1個以上 */
 const nqs = /[^'"]+/;
+/** 空白文字の連続0個以上 */
 const sp = /\s*/;
-const el = r(sp, /;/, sp);
+/** Perlの文の終了(セミコロンとその前の空白文字) */
+const el = r(sp, /;/);
+/** モジュール名(クオートないことがある(ファットカンマのときとか)) */
 const moduleName = r(nqs, or, q, nqs, q);
+/** onにつくphase名(クオートないことがある(ファットカンマのときとか)) */
 const phase = r(nqs, or, q, nqs, q);
+/** バージョンの指定(数字) */
 const versionNum = /[0-9.]+/;
+/** バージョンの指定(文字列( `'> 0.5.0, < 1.0.0'` のようなこともある)) */
 const versionStr = r(q, nqs, q);
+/** バージョンの指定(数字か文字列) */
 const version = r(versionNum, or, versionStr);
+/** カンマかファットカンマ(とその前後の空白文字) */
 const comma = r(sp, /(?:,|=>)/, sp);
-const moduleNameVersion = r(moduleName, comma, version);
-const moduleOrmoduleNameVersion = r(moduleName, or, moduleNameVersion);
-const makeModuleStatement = (name: string) =>
-  r(sp, new RegExp(name), sp, moduleOrmoduleNameVersion, el);
-const requires = makeModuleStatement('requires');
-const author_requires = makeModuleStatement('author_requires');
-const configureRequires = makeModuleStatement('configureRequires');
-const test_requires = makeModuleStatement('test_requires');
-const conflicts = makeModuleStatement('conflicts');
-const recommends = makeModuleStatement('recommends');
-const moduleStatements = r(
-  requires,
-  or,
-  author_requires,
-  or,
-  configureRequires,
-  or,
-  test_requires,
-  or,
-  conflicts,
-  or,
-  recommends
+/** モジュール名だけ, またはモジュール名とバージョン指定 */
+const moduleOrmoduleNameVersion = r(
+  moduleName,
+  rrepeat(r(comma, version), '?')
 );
-const comment = r(sp, /#[^\n]+\n/);
-const sub = r(
+/** モジュールの指定の文(種類がいろいろある) */
+const moduleStatements = rcapture(
+  /(?:requires|author_requires|configureRequires|test_requires|conflicts|recommends)/,
   sp,
-  /sub\s*\{/,
-  rrepeat(r(sp, r(moduleStatements, or, comment), sp), '*'),
-  /\}/,
+  moduleOrmoduleNameVersion,
   el
 );
-const on = r(sp, /on/, sp, phase, comma, sub, el);
+/** コメント文 */
+const comment = /#[^\n]*(?:\n|$)/;
+/** サブルーチン(中にモジュールの指定の文が0個以上入る) */
+const sub = r(
+  /sub\s*\{/,
+  rrepeat(r(sp, r(moduleStatements, or, comment)), '*'),
+  /\s*\}/
+);
+/** on(phaseとsubが指定されてセミコロンが続く) */
+const on = r(/on/, sp, phase, comma, sub, el);
+/**
+ * cpanfileに対して全体を見れる正規表現
+ * - 地べたの文とonがひとつずつ取れる
+ */
 const cpanfileRegExp = new RegExp(
-  r(moduleStatements, or, on, or, comment),
+  r(sp, r(moduleStatements, or, on, or, comment)),
   'g'
 );
 
