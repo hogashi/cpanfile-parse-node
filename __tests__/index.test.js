4ea16e45f9d7ffb90233d83033d858ada99e0efa
hogashi <hogextend@gmail.com>
2021-08-21 21:16:44 +0900

    add test from Module::CPANfile

--- __tests__/index.test.js
+++
@@ -1,5 +0,0 @@
-const index = require('../index');
-
-it('parse cpanfile', () => {
-  return expect(index.parseCpanfile('./__tests__/cpanfile')).resolves.toMatchSnapshot();
-});
---
+++ __tests__/parser.test.js
@@ -0,0 +1,42 @@
+const parser = require('../parser');
+
+it('parse cpanfile', () => {
+  const cpanfileStr = [
+    `configure_requires 'ExtUtils::MakeMaker', 5.5;`,
+    ``,
+    `requires 'DBI';`,
+    `requires 'Plack', '0.9970';`,
+    `conflicts 'Moose', '< 0.8';`,
+    ``,
+    `on 'test' => sub {`,
+    `    requires 'Test::More';`,
+    `};`,
+    ``,
+    `on 'develop' => sub {`,
+    `    requires 'Catalyst::Runtime', '> 5.8000, < 5.9';`,
+    `    recommends 'Catalyst::Plugin::Foo';`,
+    `};`,
+    ``,
+    `test_requires 'Test::Warn', 0.1;`,
+    `author_requires 'Module::Install', 0.99;`,
+  ].join('\n');
+  return expect(parser.parse(cpanfileStr)).toMatchObject({
+    configure: {
+      requires: { 'ExtUtils::MakeMaker': '5.5' },
+    },
+    runtime: {
+      requires: { Plack: '0.9970', DBI: 0 },
+      conflicts: { Moose: '< 0.8' },
+    },
+    test: {
+      requires: { 'Test::More': 0, 'Test::Warn': '0.1' },
+    },
+    develop: {
+      requires: {
+        'Catalyst::Runtime': '> 5.8000, < 5.9',
+        'Module::Install': '0.99',
+      },
+      recommends: { 'Catalyst::Plugin::Foo': 0 },
+    },
+  });
+});
